44020ca80ae485d611dd8f233f340472a4d4cf35
andre sun <andsun9999@gmail.com>
2026-09-01 22:06:57 -0400

    Add RAII wrappers for pinned, device, stream and event

---
+++ include/frame_arena/cuda_resource.hpp
@@ -0,0 +1,97 @@
+#pragma once
+
+#include <cuda_runtime.h>
+
+#include <cstddef>
+#include <utility>
+
+namespace frame_arena {
+
+class PinnedBuffer {
+public:
+  PinnedBuffer() noexcept = default;
+  explicit PinnedBuffer(std::size_t bytes);
+  ~PinnedBuffer() noexcept;
+
+  PinnedBuffer(const PinnedBuffer&) = delete;
+  PinnedBuffer& operator=(const PinnedBuffer&) = delete;
+
+  PinnedBuffer(PinnedBuffer&& other) noexcept;
+  PinnedBuffer& operator=(PinnedBuffer&& other) noexcept;
+
+  void* data() const noexcept { return data_; }
+  std::size_t size() const noexcept { return size_; }
+
+private:
+  void release() noexcept;
+
+  void* data_ = nullptr;
+  std::size_t size_ = 0;
+};
+
+class DeviceBuffer {
+public:
+  DeviceBuffer() noexcept = default;
+  explicit DeviceBuffer(std::size_t bytes);
+  ~DeviceBuffer() noexcept;
+
+  DeviceBuffer(const DeviceBuffer&) = delete;
+  DeviceBuffer& operator=(const DeviceBuffer&) = delete;
+
+  DeviceBuffer(DeviceBuffer&& other) noexcept;
+  DeviceBuffer& operator=(DeviceBuffer&& other) noexcept;
+
+  void* data() const noexcept { return data_; }
+  std::size_t size() const noexcept { return size_; }
+
+private:
+  void release() noexcept;
+
+  void* data_ = nullptr;
+  std::size_t size_ = 0;
+};
+
+class CudaStream {
+public:
+  CudaStream();
+  ~CudaStream() noexcept;
+
+  CudaStream(const CudaStream&) = delete;
+  CudaStream& operator=(const CudaStream&) = delete;
+
+  CudaStream(CudaStream&& other) noexcept;
+  CudaStream& operator=(CudaStream&& other) noexcept;
+
+  cudaStream_t get() const noexcept { return stream_; }
+  void synchronize() const;
+
+private:
+  void release() noexcept;
+
+  cudaStream_t stream_ = nullptr;
+};
+
+class CudaEvent {
+public:
+  explicit CudaEvent(unsigned flags = cudaEventDisableTiming);
+  ~CudaEvent() noexcept;
+
+  CudaEvent(const CudaEvent&) = delete;
+  CudaEvent& operator=(const CudaEvent&) = delete;
+
+  CudaEvent(CudaEvent&& other) noexcept;
+  CudaEvent& operator=(CudaEvent&& other) noexcept;
+
+  cudaEvent_t get() const noexcept { return event_; }
+  void record(cudaStream_t stream) const;
+  cudaError_t query() const noexcept;
+  void synchronize() const;
+  float elapsed_ms_since(const CudaEvent& earlier) const;
+
+private:
+  void release() noexcept;
+
+  cudaEvent_t event_ = nullptr;
+};
+
+}
---
+++ src/cuda_resource.cpp
@@ -0,0 +1,125 @@
+#include "frame_arena/cuda_resource.hpp"
+#include "frame_arena/cuda_error.hpp"
+
+#include <cassert>
+#include <cstdio>
+
+namespace frame_arena {
+
+namespace {
+
+void warn_on_failure(cudaError_t code, const char* operation) noexcept {
+  if (code == cudaSuccess) return;
+  std::fprintf(stderr, "%s failed during cleanup: %s\n", operation, cudaGetErrorString(code));
+  assert(false && "CUDA cleanup failed");
+}
+
+}
+
+PinnedBuffer::PinnedBuffer(std::size_t bytes) : size_(bytes) {
+  CUDA_CHECK(cudaHostAlloc(&data_, bytes, cudaHostAllocDefault));
+}
+
+PinnedBuffer::~PinnedBuffer() noexcept { release(); }
+
+PinnedBuffer::PinnedBuffer(PinnedBuffer&& other) noexcept
+    : data_(std::exchange(other.data_, nullptr)), size_(std::exchange(other.size_, 0)) {}
+
+PinnedBuffer& PinnedBuffer::operator=(PinnedBuffer&& other) noexcept {
+  if (this != &other) {
+    release();
+    data_ = std::exchange(other.data_, nullptr);
+    size_ = std::exchange(other.size_, 0);
+  }
+  return *this;
+}
+
+void PinnedBuffer::release() noexcept {
+  if (data_ != nullptr) warn_on_failure(cudaFreeHost(data_), "cudaFreeHost");
+  data_ = nullptr;
+  size_ = 0;
+}
+
+DeviceBuffer::DeviceBuffer(std::size_t bytes) : size_(bytes) {
+  CUDA_CHECK(cudaMalloc(&data_, bytes));
+}
+
+DeviceBuffer::~DeviceBuffer() noexcept { release(); }
+
+DeviceBuffer::DeviceBuffer(DeviceBuffer&& other) noexcept
+    : data_(std::exchange(other.data_, nullptr)), size_(std::exchange(other.size_, 0)) {}
+
+DeviceBuffer& DeviceBuffer::operator=(DeviceBuffer&& other) noexcept {
+  if (this != &other) {
+    release();
+    data_ = std::exchange(other.data_, nullptr);
+    size_ = std::exchange(other.size_, 0);
+  }
+  return *this;
+}
+
+void DeviceBuffer::release() noexcept {
+  if (data_ != nullptr) warn_on_failure(cudaFree(data_), "cudaFree");
+  data_ = nullptr;
+  size_ = 0;
+}
+
+CudaStream::CudaStream() {
+  CUDA_CHECK(cudaStreamCreateWithFlags(&stream_, cudaStreamNonBlocking));
+}
+
+CudaStream::~CudaStream() noexcept { release(); }
+
+CudaStream::CudaStream(CudaStream&& other) noexcept
+    : stream_(std::exchange(other.stream_, nullptr)) {}
+
+CudaStream& CudaStream::operator=(CudaStream&& other) noexcept {
+  if (this != &other) {
+    release();
+    stream_ = std::exchange(other.stream_, nullptr);
+  }
+  return *this;
+}
+
+void CudaStream::synchronize() const { CUDA_CHECK(cudaStreamSynchronize(stream_)); }
+
+void CudaStream::release() noexcept {
+  if (stream_ != nullptr) warn_on_failure(cudaStreamDestroy(stream_), "cudaStreamDestroy");
+  stream_ = nullptr;
+}
+
+CudaEvent::CudaEvent(unsigned flags) {
+  CUDA_CHECK(cudaEventCreateWithFlags(&event_, flags));
+}
+
+CudaEvent::~CudaEvent() noexcept { release(); }
+
+CudaEvent::CudaEvent(CudaEvent&& other) noexcept
+    : event_(std::exchange(other.event_, nullptr)) {}
+
+CudaEvent& CudaEvent::operator=(CudaEvent&& other) noexcept {
+  if (this != &other) {
+    release();
+    event_ = std::exchange(other.event_, nullptr);
+  }
+  return *this;
+}
+
+void CudaEvent::record(cudaStream_t stream) const { CUDA_CHECK(cudaEventRecord(event_, stream)); }
+
+cudaError_t CudaEvent::query() const noexcept { return cudaEventQuery(event_); }
+
+void CudaEvent::synchronize() const { CUDA_CHECK(cudaEventSynchronize(event_)); }
+
+float CudaEvent::elapsed_ms_since(const CudaEvent& earlier) const {
+  float ms = 0.0f;
+  CUDA_CHECK(cudaEventElapsedTime(&ms, earlier.event_, event_));
+  return ms;
+}
+
+void CudaEvent::release() noexcept {
+  if (event_ != nullptr) warn_on_failure(cudaEventDestroy(event_), "cudaEventDestroy");
+  event_ = nullptr;
+}
+
+}
